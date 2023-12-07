9e97224d3fdc4fefb2ff3806a2aa8e2900a5435b
Vijay Reddy <vijaynagireddy76@gmail.com>
2023-12-07 16:38:27 +0530

    Second commit with Models and db updates

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 # Create your views here.
+
+
+def home(request):
+    context  = {
+        'posts':Post.objects.all(),
+        'title': 'Vijay'
+    }
+    return render(request, "blog/home.html", context)
+
+
+def about(request):
+    # print("project:", request)
+    context= {'project':"request"}
+    return render(request,"blog/about.html", context=context)
 
